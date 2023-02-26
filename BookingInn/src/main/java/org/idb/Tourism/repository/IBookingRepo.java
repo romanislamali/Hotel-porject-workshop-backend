db52cd9aeb9607b25434d24e6f3e55ec8bb1961f
Md. Roman Islam Ali <107925118+romanislamali@users.noreply.github.com>
2023-02-27 01:07:48 +0600

    push

--- BookingInn/src/main/java/org/idb/Tourism/repository/IBookingRepo.java
+++ BookingInn/src/main/java/org/idb/Tourism/repository/IBookingRepo.java
@@ -2,10 +2,16 @@ package org.idb.Tourism.repository;
 
 import org.idb.Tourism.entity.Booking;
 
+import org.idb.Tourism.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IBookingRepo extends JpaRepository<Booking, Integer> {
+    @Query("select max(b.bid) from Booking b")
+    public Integer findMaxBookingId();
 
 }
